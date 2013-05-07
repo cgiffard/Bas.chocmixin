8815bb6a916fae0ff04fe06f9ec10d53f67c168a
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-05-07 15:17:27 +1000

    Made some progress. Setup window basically complete. Progress and display windows still to go

--- init.js
+++ init.js
@@ -8,8 +8,9 @@ Hooks.addMenuItem("Actions/Run Sheet With Bas", "cmd-b", function() {
 
 function loadSettingsWindow() {
 	
-	var options	= {},
-		initWindow = new Window();
+	var storage		= Storage.persistent(),
+		options		= {},
+		initWindow	= new Window();
 	
 	initWindow.title = "Initialise Bas Scan";
 	initWindow.useDefaultCSS = false;
@@ -29,6 +30,23 @@ function loadSettingsWindow() {
 	initWindow.run();
 	initWindow.show();
 	
+	if (storage.get("urls"))
+		initWindow.evalExpr("$('#urls')[0].value = " +
+			JSON.stringify(storage.get("urls")));
+	
+	if (storage.get("crawl"))
+		initWindow.evalExpr("$('#crawl')[0].checked = true");
+	
+	if (storage.get("limitcrawl"))
+		initWindow.evalExpr("$('#limitcrawl')[0].checked = true");
+	
+	if (storage.get("crawlthreshhold"))
+		initWindow.evalExpr("$('#crawlthreshhold')[0].value = " +
+			parseInt(storage.get("crawlthreshhold"),10));
+	
+	if (storage.get("dieonfirst"))
+		initWindow.evalExpr("$('#dieonfirst')[0].checked = true");
+	
 	initWindow.onButtonClick = function(name) {
 		
 		if (name === "Cancel")
@@ -36,12 +54,19 @@ function loadSettingsWindow() {
 		
 		options = initWindow.evalExpr("enumerateOptions()");
 		
+		for (var option in options) {
+			if (options.hasOwnProperty(option)) {
+				storage.set(option,options[option]);
+			}
+		}
+		
 		if (!options.urls || !options.urls.replace(/\s/ig,"").length)
 			return Alert.show(
-						"You must specify at least one URL to request.",
-						"And they should be valid URLs, but I won't hold your hand.",
-						["Well, OK then."]);
+					"You must specify at least one URL to request.",
+					"And they should be valid URLs, but I won't hold your hand.",
+					["Well, OK then."]);
 		
+		initWindow.close();
 		getBasDocument(options);
 	}
 }
@@ -56,17 +81,17 @@ function getBasDocument(options) {
 			"You need to open a valid Bas sheet before running a test.",
 			["Well, OK then."]);
 	
+	var fetcher = runBasSheet(options,basText);
+	
 	Alert.notify({
-		title: "Commencing tests...",
-		subtitle: "Bas will now test your requested URLs.",
-		body: "Use the goto keyword 10 times in the same file",
-		button: "Stop",
+		title:		"Commencing tests",
+		subtitle:	"Bas will now request the specified URLs.",
+		body:		"Results will appear soon.",
+		button:		"Stop",
 		callback: function () {
-			// Do something when the "Revert" button is clicked
+			fetcher.stop();
 		}
 	});
-	
-	runBasSheet(options,basText);
 }
 
 function runBasSheet(options,text) {
@@ -84,8 +109,47 @@ function runBasSheet(options,text) {
 	
 	testSuite.loadSheet(new Buffer(text))
 		.yep(fetcher.go);
-		
+	
+	
+	
 	fetcher.on("complete",function() {
 		
 	});
+	
+	testSuite.on("start",function(url) {
+		Alert.notify({
+			title:		"Testing resource",
+			body:		url,
+			button:		"Stop",
+			callback: function () {
+				fetcher.stop();
+			}
+		});
+	});
+	
+	testSuite.on("assertionfailed",function(errors,assertion) {
+		Alert.notify({
+			title:		"Assertion failed",
+			subtitle:	String(assertion),
+			body:		String(errors),
+			button:		"Stop",
+			callback: function () {
+				fetcher.stop();
+			}
+		});
+	});
+	
+	testSuite.on("end",function() {
+		Alert.notify({
+			title:		"Completed Test Suite",
+			subtitle:	"Completed testing resource.",
+			body:		url,
+			button:		"Stop",
+			callback: function () {
+				fetcher.stop();
+			}
+		});
+	});
+	
+	return fetcher;
 }
